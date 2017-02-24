9fc25cca13609c1f52f13270304934f3a367f523
Marcelorvp <ipmicrobious@gmail.com>
2017-02-24 20:00:45 +0000

    Fixed

--- usuario.sh
+++ usuario.sh
@@ -8,11 +8,11 @@ echo -n "El canal fue creado en: "
 GET https://channelmeter.com/channels/$usuario | grep "Created Date" | cut -d '&' -f 4 | cut -d '>' -f 8 | cut -d '<' -f 1
 echo " "
 echo -n "Total de vídeos subidos: "
-GET https://channelmeter.com/channels/elrubiusomg | grep "Created Date" | cut -d '&' -f 4 | cut -d '<' -f 14 | cut -d '>' -f 2
+GET https://channelmeter.com/channels/$usuario | grep "Created Date" | cut -d '&' -f 4 | cut -d '<' -f 14 | cut -d '>' -f 2
 echo " "
 echo -n "Visitas totales en el canal: "
-GET https://channelmeter.com/channels/elrubiusomg | grep "Created Date" | cut -d '&' -f 4 | cut -d '<' -f 20 | cut -d '>' -f 2
+GET https://channelmeter.com/channels/$usuario | grep "Created Date" | cut -d '&' -f 4 | cut -d '<' -f 20 | cut -d '>' -f 2
 echo " "
 echo -n "Suscriptores actuales en el canal: "
-GET https://channelmeter.com/channels/elrubiusomg | grep "Created Date" | cut -d '&' -f 4 | cut -d '<' -f 26 | cut -d '>' -f 2
+GET https://channelmeter.com/channels/$usuario | grep "Created Date" | cut -d '&' -f 4 | cut -d '<' -f 26 | cut -d '>' -f 2
 echo " "
